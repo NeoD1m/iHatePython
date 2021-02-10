362d88e99eb13bf2f3e724e06ec711b5abf85138
NeoD1m <duffman7915492@gmail.com>
2021-02-10 12:40:19 +0800

    pr4 done

---
+++ pr4/main.py
@@ -0,0 +1,11 @@
+def func(n):
+    if n == 0:
+        return 7
+    else:
+        return (1/95)*func(n-1) + (1/85)*((func(n-1))**2)
+
+
+
+if __name__ == '__main__':
+    print("%.2e" % func(16))
+    print("%.2e" % func(10))
