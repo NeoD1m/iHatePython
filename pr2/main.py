8946c14bf16a1ded203498a391598a52ea813535
NeoD1m <duffman7915492@gmail.com>
2021-02-10 11:52:54 +0800

    pr2 done

---
+++ pr2/main.py
@@ -0,0 +1,16 @@
+import math
+
+
+def func(x):
+    if x < 86:
+        return (math.e ** (x ** 3)) + (x ** 7)
+    elif 86 <= x & x < 160:
+        return ((x**4)/78) - ((x**2)/8)
+    elif 160 <= x & x < 188:
+        return 45 * x - x ** 8
+    elif x >= 188:
+        return math.tan(x-68*(x**7))+30*(x**8)
+
+if __name__ == '__main__':
+    print("%.2e" % func(222))
+    print("%.2e" % func(208))
