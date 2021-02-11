26349daead93a502bc52b3744442d3e1d9703149
NeoD1m <duffman7915492@gmail.com>
2021-02-11 20:33:25 +0300

    pr 3 done

--- pr3/main.py
+++ pr3/main.py
@@ -2,18 +2,19 @@ import math
 
 
 def func(n, m):
+    #print (sum2(n))
     return 92 * sum(n, m) - sum2(n)
 
 
 def sum(n, m, k=0):
-    for i in range(1, n, 1):
-        for j in range(1, m, 1):
+    for i in range(1, n+1, 1):
+        for j in range(1, m+1, 1):
             k += (math.e ** j + i ** 3)
     return k
 
 
 def sum2(n, k=0):
-    for i in range(1, n, 1):
+    for i in range(1, n+1, 1):
         k += ((math.log(i, math.e) - ((i ** 7) / 3)) + 5)
     return k
 
