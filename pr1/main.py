5ff2f50051b5b907e98eca64ac514053c1b9ccb1
NeoD1m <duffman7915492@gmail.com>
2021-02-17 18:30:39 +0300

    рефактор и pr2

--- pr1/main.py
+++ pr1/main.py
@@ -1,12 +1,43 @@
 import math
 
 
-def func(x, y):
+def f11(x, y):
     res = (97 * (y ** 6) - (math.e ** x) + 72) / (y + ((y ** 2) / 53) + 35) - math.sqrt(
         (86 * (y ** 8) + ((x ** 3) / 51)) / ((69 * (x ** 6)) + math.sin(y))) + 47 * (x ** 8) - (y ** 4) - 25
-    return res
+    return "%.2e" % res
 
 
-if __name__ == '__main__':
-    print("%.2e" % func(87, 54))
-    print("%.2e" % func(-74, -98))
+def f12(x):
+    if x < 86:
+        return (math.e ** (x ** 3)) + (x ** 7)
+    elif 86 <= x & x < 160:
+        return ((x ** 4) / 78) - ((x ** 2) / 8)
+    elif 160 <= x & x < 188:
+        return 45 * x - x ** 8
+    elif x >= 188:
+        return "%.2e" % (math.tan(x - 68 * (x ** 7)) + 30 * (x ** 8))
+
+
+def f13(n, m):
+    return "%.2e" % (92 * sum(n, m) - sum2(n))
+
+
+def sum(n, m, k=0):
+    for i in range(1, n+1, 1):
+        for j in range(1, m+1, 1):
+            k += (math.e ** j + i ** 3)
+    return k
+
+
+def sum2(n, k=0):
+    for i in range(1, n+1, 1):
+        k += ((math.log(i, math.e) - ((i ** 7) / 3)) + 5)
+    return k
+
+
+def f14(n):
+    if n == 0:
+        return 7
+    else:
+        return "%.2e" % ((1 / 95) * func(n - 1) + (1 / 85) * ((func(n - 1)) ** 2))
+
--- pr2/main.py
+++ pr2/main.py
@@ -1,17 +1,38 @@
-import math
+def f22(my_hexdata):
+    scale = 16  # equals to hexadecimal
+    num_of_bits = 8
+
+    bit_list = []
+    bit_list[:0] = bin(int(my_hexdata, scale))[2:].zfill(num_of_bits)
+
+    while len(bit_list) < 32:
+        bit_list.insert(0, "0")
+
+    bit_list2 = []
+
+    for i in range(6, 18, 1):  # B
+        bit_list2.insert(i - 7, bit_list[i])
+
+    for i in range(2, 6, 1):  # C
+        bit_list2.insert(i + 9, bit_list[i])
+
+    for i in range(0, 2, 1):  # ED
+        bit_list2.insert(i + 16, bit_list[i])
+
+    for i in range(18, 32, 1):  # A
+        bit_list2.insert(i, bit_list[i])
+
+    bitstring = ""
+    for bits in bit_list2:
+        bitstring += bits
+
+    return hex(int(bitstring, 2))
 
 
-def func(x):
-    if x < 86:
-        return (math.e ** (x ** 3)) + (x ** 7)
-    elif 86 <= x & x < 160:
-        return ((x ** 4) / 78) - ((x ** 2) / 8)
-    elif 160 <= x & x < 188:
-        return 45 * x - x ** 8
-    elif x >= 188:
-        return math.tan(x - 68 * (x ** 7)) + 30 * (x ** 8)
+def f21(list=[]):
+    return list
 
 
 if __name__ == '__main__':
-    print("%.2e" % func(222))
-    print("%.2e" % func(208))
+    print(f22("0xd139a832"))
+    print(f22("0x6afb6fe4"))
