24d70ea4138e324838d683373b72f3886552b6b8
NeoD1m <duffman7915492@gmail.com>
2021-02-19 01:53:34 +0300

    пр3 почти готова

--- pr2/main.py
+++ pr2/main.py
@@ -96,6 +96,25 @@ def f23(list=[[]]):
             if "-" in list[i][j]:
                 list[i][j] = list[i][j].replace("-", "/")
 
+    for i in range(len(list)):  # Выделяем фамилию в отдельную клетку
+        for j in range(len(list[i])):
+            if "!" in list[i][j]:
+                list[i].append(list[i][j].split("!", 1)[1])
+                list[i][j+1] = list[i][j+1].split(" ", 1)[0]
+
+    for i in range(len(list)):  # Меняем фамилию и проценты местами
+        for j in range(len(list[i])-1):
+            if "!" in list[i][j]:
+                temp = list[i][j+1]
+                list[i][j+1] = list[i][j]
+                list[i][j] = temp
+
+    for i in range(len(list)):  # Переводим числа в проценты
+        for j in range(len(list[i])):
+            if "!" in list[i][j]:
+                list[i][j] = list[i][j].split("!", 1)[0]
+                list[i][j] = str(int(round(float(list[i][j]), 2)*100))+"%"
+
     listMyList(list)
 
 
@@ -103,8 +122,8 @@ if __name__ == '__main__':
     # print(f21(['cuda', 'c', 1996, 1995]))
     # print(f21(['pug', 'mako', 1967, 2010]))
     # print(f21(['pug', 2019, 1967, 'mako']))
-    print(f23([["besizak60@rambler.ru", None, "besizak60@rambler.ru", "2002-04-12", "0.899!Бесицак Федор"],
-               [None, None, None, None],
-               ["nocman35@yandex.ru", None, "nocman35@yandex.ru", "2002-01-27", "0.729!Ночман Артур"],
-               ["gordej5@rambler.ru", None, "gordej5@rambler.ru", " 1999-10-10", "0.560!Гибов Гордей"]
-               ]))
+    f23([["besizak60@rambler.ru", None, "besizak60@rambler.ru", "2002-04-12", "0.899!Бесицак Федор"],
+         [None, None, None, None],
+         ["nocman35@yandex.ru", None, "nocman35@yandex.ru", "2002-01-27", "0.729!Ночман Артур"],
+         ["gordej5@rambler.ru", None, "gordej5@rambler.ru", " 1999-10-10", "0.560!Гибов Гордей"]
+         ])
